a9b176c7febd3c72a7aecca2b1479d49562565fb
xgrois <67983850+xgrois@users.noreply.github.com>
2020-12-09 12:53:47 +0100

    Add project files.

---
+++ TowerOfHanoi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TowerOfHanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //        |         |         | 
+            //        |         |         | 
+            //       |1|        |         | 
+            //      | 2 |       |         | 
+            //     |  3  |      |         |  
+            //  --------------------------------
+            //        A         B         C
+
+            Hanoi(3, 'A', 'B', 'C');
+        }
+
+
+        static void Hanoi(int n, char src, char aux, char dst)
+        {
+            if (n > 0)
+            {
+                Hanoi(n - 1, src, dst, aux);
+                Console.WriteLine($"Move disk {n}: {src} --> {dst}");
+                Hanoi(n - 1, aux, src, dst);
+            }
+
+        }
+
+    }
+}
